def5f3796806fc9cc6b1275d1c4725ffdf4b2860
freezer3 <44923519+freezer3@users.noreply.github.com>
2020-04-27 18:55:31 +0200

    new functions and features

--- mutations_location.py
+++ mutations_location.py
@@ -12,7 +12,7 @@ DF['date'] = pd.to_datetime(DF['date']) #converting data column to data type
 
 DF.sort_values(by ='date',ascending = False, inplace= True)
 DF = DF.head((len(DF)-6)) #deleting outlier data
-str = 'S_gene_translation_changes'
+string = 'S_gene_translation_changes'
 # DF['orf1ab_mutations'] = DF.apply(lambda
 #                                row: (ast.literal_eval(row.orf1ab_mutations)),
 #                            axis=1)
@@ -27,17 +27,131 @@ def change_format(list):
     for i in range(max_val + 10):
         value_counter.append(list.count(i))
     return value_counter
-
-plt.style.use('ggplot')
-for cols in DF:
-    hist_data = []
-    if '_mutations' in cols or '_changes' in cols:
+# plt.style.use('ggplot')
+# fig, ax = plt.subplots(nrows=20,ncols=1, sharex=True, sharey=True)
+mut = ['E_gene_mutations', 'M_gene_mutations', 'S_gene_mutations', 'N_gene_mutations', 'orf1ab_mutations', 'ORF3a_mutations',
+       'ORF6_mutations', 'ORF7_mutations', 'ORF8_mutations', 'ORF10_mutations']
+trans = ['E_gene_translation_changes', 'M_gene_translation_changes', 'S_gene_translation_changes', 'N_gene_translation_changes',
+         'orf1ab_translation_changes', 'ORF3a_translation_changes', 'ORF6_translation_changes', 'ORF7_translation_changes', 'ORF8_translation_changes',
+         'ORF10_translation_changes']
+def get_lists_colorbar():
+    lists = []
+    for cols in mut:
+        hist_data=[]
         for location in DF[cols]:
             location = ast.literal_eval(location)
-            if len(location) !=0:
+            if len(location) != 0:
                 for list in location:
-                     hist_data.append(list[0])
+                    hist_data.append(list[0])
         new_list = change_format(hist_data)
-        #now u have list format which u wanted
-        break
-        plt.savefig('plots/distributions/' + cols + '.png')
+        lists.append(new_list)
+    return lists
+
+def llwrite(list,filename='mutation_density.pg'):
+    with open(filename, 'w') as f:
+        for _list in list:
+            f.write('#' + '\n')
+            for int in _list:
+                f.write(str(int) + '\n')
+
+def llread(filename='mutation_density.pg'):
+    with open(filename, 'r') as f:
+        result=[]
+        i=0
+        for line in f:
+            if line == '#\n':
+                if i==0:
+                    i+=1
+                else:
+                    result.append(a)
+                a=[]
+            else:
+                a.append(int(line[:-1]))
+        result.append(a)
+        return(result)
+
+def colorbar():
+    i=0
+    data=llread()
+    fig, axs =plt.subplots(10,sharey=True,sharex=False)
+    a=[]
+    for list in data:
+        label = mut[i][:-10]
+        arr = np.array(list)
+        arr = np.stack((arr,arr),axis=0) #thicc1
+        # arr = np.concatenate((arr, arr), axis=0)
+        # arr = np.concatenate((arr, arr), axis=0)
+        # arr = np.concatenate((arr, arr), axis=0) #thicc4
+        # ax.subplot(20,1,t)
+        im = axs[i].imshow(arr,aspect='auto',cmap='plasma')
+        axs[i].set_ylabel(label, rotation=0, labelpad=25)
+        # axs[i].xaxis.labelpad=100
+        # axs[i].set_yticklabels([])
+        # plt.ylabel(cols[:-10])
+        # axs[i].axis('off')
+        axs[i].axes.get_xaxis().set_ticks([])
+        axs[i].axes.get_yaxis().set_ticks([])
+        i+=1
+    fig.colorbar(im, ax=axs)
+    # plt.yticks([])
+    # plt.xticks([])
+    plt.suptitle('Mutation density in genes of SARS-CoV-2 virus',y=0.95)
+    plt.savefig('mutation_density.png')
+    plt.show()
+
+def colorbar_s():
+    i=0
+    data=llread()
+    fig, ax = plt.subplots()
+    # label = mut[i][:-10]
+    arr = np.array(data[2])
+    arr = np.stack((arr,arr),axis=0) #thicc1
+    # arr = np.concatenate((arr, arr), axis=0)
+    # arr = np.concatenate((arr, arr), axis=0)
+    # arr = np.concatenate((arr, arr), axis=0) #thicc4
+    # ax.subplot(20,1,t)
+    im = ax.imshow(arr,aspect=200,cmap='plasma')
+    # ax.set_ylabel(label, rotation=0, labelpad=25)
+    # axs[i].xaxis.labelpad=100
+    # axs[i].set_yticklabels([])
+    # plt.ylabel(cols[:-10])
+    # axs[i].axis('off')
+    # ax.axes.get_xaxis().set_ticks([])
+    ax.axes.get_yaxis().set_ticks([])
+    ax.set_xlabel('S gene length')
+    fig.colorbar(im, ax=ax)
+    # plt.yticks([])
+    # plt.xticks([])
+    plt.suptitle('Mutation density in S gene of SARS-CoV-2 virus',y=0.95)
+    plt.savefig('mutation_density_s.png')
+    plt.show()
+
+# colorbar_s()
+# for cols in DF:
+#     hist_data = []
+#     if '_mutations' in cols or '_changes' in cols:
+#         for location in DF[cols]:
+#             location = ast.literal_eval(location)
+#             if len(location) !=0:
+#                 for list in location:
+#                      hist_data.append(list[0])
+#         new_list = change_format(hist_data)
+
+        # arr1 = np.array(new_list)
+        # arr = np.stack((arr1,arr1),axis=0)
+        # arr = np.concatenate((arr, arr), axis=0)
+        # arr = np.concatenate((arr, arr), axis=0)
+        # arr = np.concatenate((arr, arr), axis=0)
+        # plt.subplot(20,1,i)
+        # ax.imshow(arr)
+        # ax.colorbar()
+        # plt.ylabel(cols[:-10])
+        # i+=1
+        # print(i)
+        # plt.show()
+        # print(arr)
+        # break
+        # plt.savefig('plots/distributions/' + cols + '.png')
+# print(columny)
+# plt.colorbar()
+# plt.show()
