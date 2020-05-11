a492e195009dfc6ac05f9efa619905aa3aaf8e86
grzegorz <xd@gmail.co>
2020-05-11 15:39:45 +0200

    Added making comparison to reference genome - use True as 3rd argument in distances_tocsv to generate old kind of csv :)

--- calculations/python/tocsv.py
+++ calculations/python/tocsv.py
@@ -5,7 +5,8 @@ import itertools
 from calculations.python.paths import *
 
 
-def distances_tocsv(file=data_path(EXTRA_COMPARISONS_RESULTS), output_file=data_path(COMPARISONS_CSV)):
+def distances_tocsv(file=data_path(EXTRA_COMPARISONS_RESULTS), output_file=data_path(COMPARISONS_CSV),
+                    compare_to_ref=False):
     with open(file, 'r') as file:
         data_list = []
 
@@ -21,17 +22,28 @@ def distances_tocsv(file=data_path(EXTRA_COMPARISONS_RESULTS), output_file=data_
             if type(eval(line)) == type(()):
                 temp_dict = (eval(line))[0]
 
+                if compare_to_ref:
+                    temp_dict_ref = eval(line)[1]
+
             else:
                 temp_dict = eval(line)
-            print(temp_dict)
 
             temp_dict2 = {gene['name'] + '_' + k: gene[k] for gene in temp_dict['genes'] for k in gene if k != 'name'}
+            if compare_to_ref:
+                temp_dict_ref_2 = {gene['name'] + '_' + k: gene[k] for gene in temp_dict_ref['genes'] for k in gene if
+                                   k != 'name'}
+
+                temp_dict2 = dict(temp_dict2, **temp_dict_ref_2)
+                temp_dict2['compared'] = temp_dict_ref['compared']
+                temp_dict2['distance'] = temp_dict_ref['distance']
+                temp_dict2['date'] = temp_dict_ref['compared'].split('|')[2]
             del temp_dict['genes']
             for k, v in temp_dict.items():
                 temp_dict2[k] = v
-
             data_list.append(temp_dict2)
         csv_columns = data_list[0].keys()
+        if compare_to_ref:
+            output_file = '../../data/genome_data.csv'
         with open(output_file, 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
             writer.writeheader()
@@ -40,8 +52,8 @@ def distances_tocsv(file=data_path(EXTRA_COMPARISONS_RESULTS), output_file=data_
     # df = pd.read_csv(output_file)
 
 
-distances_tocsv()
-#distances_tocsv(data_path(CALCULATED_GENOMES_DATA), data_path(DISTANCES_CSV))
+# distances_tocsv()
+distances_tocsv(data_path(CALCULATED_GENOMES_DATA), data_path(DISTANCES_CSV), True)
 
 
 def merge_csv(file1, file2, output_file):
@@ -49,18 +61,14 @@ def merge_csv(file1, file2, output_file):
     df2 = pd.read_csv(file2)  # 2 jest mniejsza
 
     df2.rename(columns={'compared': 'header'}, inplace=True)
-    df2['date'] = df2.apply(lambda row: row.header.split('|')[2], axis = 1 )
+    df2['date'] = df2.apply(lambda row: row.header.split('|')[2], axis=1)
 
     common = df1.merge(df2, on='header', how='inner')
     common.to_csv(output_file)
 
 
-
-merge_csv(data_path(DISTANCES_CSV), data_path(COMPARISONS_CSV), data_path(MERGED_CSV))
-
-
+# merge_csv(data_path(DISTANCES_CSV), data_path(COMPARISONS_CSV), data_path(MERGED_CSV))
 
 if __name__ == "__main__":
     pass
     # merge_csv('genome_data-test.csv', 'Test-new-format.csv')
-
