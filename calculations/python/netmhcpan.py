724cd45fcd0304cdb718148453107167abc24910
michalborkowski96 <michalborkowski96@yahoo.pl>
2020-05-28 21:47:17 +0200

    netmhcpan

---
+++ calculations/python/netmhcpan.py
@@ -0,0 +1,121 @@
+from calculations.python.paths import *
+from calculations.python.find_tests import load_genomes as load_genomes_raw
+import ast, tempfile, subprocess, sys
+
+MAX_ENTRIES = 100
+NETMHCPAN_LOCATION = data_path('netMHCpan')
+MHC_ALLELE = 'HLA-A02:01'
+PEPTIDE = False
+
+def get_genome_part_to_analyze(genome, contents):
+    for gene in genome[0]["genes"]:
+        if gene["name"] == "E_gene":
+            return contents[gene["begin"]:gene["begin"] + 8]
+    raise ValueError
+
+def load_genomes():
+    for v, e in enumerate(load_genomes_raw()):
+        if v > MAX_ENTRIES:
+            break
+        yield e
+
+class NetMHCpanResult:
+    fields = ["pos", "core", "of", "gp", "gl","ip", "il", "icore", "score_el", "bindlevel"]
+
+    def __init__(self, pos, core, of, gp, gl, ip, il, icore, score_el, bindlevel):
+        self.pos = pos
+        self.core = core
+        self.of = of
+        self.gp = gp
+        self.gl = gl
+        self.ip = ip
+        self.il = il
+        self.icore = icore
+        self.score_el = score_el
+        self.bindlevel = bindlevel
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def to_dict(self):
+        return {name: getattr(self, name) for name in NetMHCpanResult.fields}
+
+def load_netmhcpan_results(file) -> [NetMHCpanResult]:
+    results = []
+    f = (line for line in file if len(line.rstrip().lstrip()) > 0 and not line.startswith('#'))
+    file = iter(f)
+    next(file)
+    for _ in file:
+        next(file) #skip dashes
+        next(file) #skip labels
+        line = next(file) #skip dashes
+        data = line.split()
+        data = [e for e in data if len(e) > 0]
+        if len(data) != 13:
+            print(line)
+            raise ValueError("Wrong format?")
+        results.append(NetMHCpanResult(int(data[0]), data[3], int(data[4]), int(data[5]), int(data[6]), int(data[7]), int(data[8]), data[9], float(data[11]), float(data[12])))
+        line = next(file)
+        if not line.startswith('-'):
+            raise ValueError("Many entries?")
+        next(file)
+        next(file)
+    return results
+
+'''
+---------------------------------------------------------------------------------------------------------------------------
+ Pos         MHC        Peptide      Core Of Gp Gl Ip Il        Icore        Identity  Score_EL %Rank_EL BindLevel
+---------------------------------------------------------------------------------------------------------------------------
+   1 HLA-A*02:01     SSQCVNLTTR SQCVNLTTR  0  1  1  0  0   SSQCVNLTTR           asdf0 0.0001840   34.071
+   1 HLA-A*02:01      SSQCVNLTT SSQCVNLTT  0  0  0  0  0    SSQCVNLTT           asdf0 0.0001460   37.000
+   2 HLA-A*02:01      SQCVNLTTR SQCVNLTTR  0  0  0  0  0    SQCVNLTTR           asdf0 0.0016100   14.832
+   1 HLA-A*02:01       SSQCVNLT SSQ-CVNLT  0  0  0  3  1     SSQCVNLT           asdf0 0.0000100   75.000
+   2 HLA-A*02:01       SQCVNLTT SQC-VNLTT  0  0  0  3  1     SQCVNLTT           asdf0 0.0000640   48.250
+   3 HLA-A*02:01       QCVNLTTR QCVN-LTTR  0  0  0  4  1     QCVNLTTR           asdf0 0.0000220   63.889
+---------------------------------------------------------------------------------------------------------------------------
+
+Protein asdf0. Allele HLA-A*02:01. Number of high binders 0. Number of weak binders 0. Number of peptides 6
+
+-----------------------------------------------------------------------------------
+'''
+
+with open(data_path(CALCULATED_GENOMES_DATA), 'r') as genomes, tempfile.NamedTemporaryFile('w') as temp_input, tempfile.NamedTemporaryFile('r') as temp_output:
+    next(genomes)
+    substrings_length = None
+    skipped = set()
+    for counter, e in enumerate(zip(genomes, load_genomes())):
+        line, raw_genome = e
+        header, contents = raw_genome
+        genome = ast.literal_eval(line)
+        try:
+            part = get_genome_part_to_analyze(genome, contents)
+        except ValueError:
+            skipped.add(counter)
+            continue
+        print(">a", file=temp_input)
+        if substrings_length is not None:
+            if len(part) != substrings_length:
+                raise ValueError("Uneven lengths")
+        else:
+            substrings_length = len(part)
+        print(part, file=temp_input)
+    if substrings_length is None:
+        raise ValueError("Empty input or all skipped")
+    if substrings_length < 8:
+        raise ValueError("Lengths too short (<8)")
+    temp_input.flush()
+    print(f"Starting netMHCpan", file=sys.stderr)
+    subprocess.check_output(f'{NETMHCPAN_LOCATION} -a {MHC_ALLELE} {"-p " if PEPTIDE else ""}-l {substrings_length} -f {temp_input.name} > {temp_output.name}', shell=True)
+    print(f"netMHCpan finished", file=sys.stderr)
+    temp_output.seek(0)
+    good_results = 0
+    print(','.join(["header", "mhc"] + NetMHCpanResult.fields))
+    for result, genome in (e for counter, e in enumerate(zip(load_netmhcpan_results(temp_output), load_genomes())) if counter not in skipped):
+        good_results += 1
+        print(genome[0], end=',')
+        print(MHC_ALLELE, end=',')
+        print(','.join(str(getattr(result, field)) for field in NetMHCpanResult.fields))
+    print(f"{len(skipped)} genomes skipped, {good_results} good", file=sys.stderr)
