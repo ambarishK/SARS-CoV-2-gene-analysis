16b8ed822c0e23410583750df059cfb484587d9e
michalborkowski96 <michalborkowski96@yahoo.pl>
2020-05-29 18:17:31 +0200

    netmhcpan

--- calculations/python/netmhcpan.py
+++ calculations/python/netmhcpan.py
@@ -1,22 +1,7 @@
 from calculations.python.paths import *
-from calculations.python.find_tests import load_genomes as load_genomes_raw
-import ast, tempfile, subprocess, sys
+import tempfile, subprocess, sys
 
-MAX_ENTRIES = 4
 NETMHCPAN_LOCATION = data_path('netMHCpan')
-MHC_ALLELES = ['HLA-A02:01', 'HLA-A01:01']
-
-def get_genome_part_to_analyze(genome, contents):
-    for gene in genome[0]["genes"]:
-        if gene["name"] == "E_gene":
-            return contents[gene["begin"]:gene["begin"] + 8]
-    raise ValueError
-
-def load_genomes():
-    for v, e in enumerate(load_genomes_raw()):
-        if v > MAX_ENTRIES:
-            break
-        yield e
 
 class NetMHCpanResult:
     fields = ["pos", "core", "of", "gp", "gl","ip", "il", "icore", "score_el", "bindlevel"]
@@ -61,42 +46,17 @@ def load_netmhcpan_results(file) -> [NetMHCpanResult]:
         results.append(NetMHCpanResult(int(data[0]), data[3], int(data[4]), int(data[5]), int(data[6]), int(data[7]), int(data[8]), data[9], float(data[11]), float(data[12])))
     return results
 
-with open(data_path(CALCULATED_GENOMES_DATA), 'r') as genomes, tempfile.NamedTemporaryFile('w') as temp_input, tempfile.NamedTemporaryFile('r') as temp_output:
-    next(genomes)
-    good = 0
-    skipped = 0
-    parts = {}
-    for counter, e in enumerate(zip(genomes, load_genomes())):
-        line, raw_genome = e
-        header, contents = raw_genome
-        genome = ast.literal_eval(line)
-        try:
-            part = get_genome_part_to_analyze(genome, contents)
-            good += 1
-        except ValueError:
-            skipped += 1
-            continue
-        if len(part) < 8:
-            raise ValueError("Lengths too short (<8)")
-        if part not in parts:
-            parts[part] = [(counter, header)]
-        else:
-            parts[part].append((counter, header))
-    if len(parts) == 0:
-        raise ValueError("Empty input or all skipped")
-    for part in parts:
-        print(part, file=temp_input)
-    temp_input.flush()
+with tempfile.NamedTemporaryFile('r') as temp_output:
     print(','.join(["header", "mhc"] + NetMHCpanResult.fields))
-    for mhc in MHC_ALLELES:
+    for i in range(1, len(sys.argv)):
+        mhc = sys.argv[i]
         print(f"Starting netMHCpan {mhc}", file=sys.stderr)
-        subprocess.check_output(f'{NETMHCPAN_LOCATION} -a {mhc} -p -f {temp_input.name} > {temp_output.name}', shell=True)
+        subprocess.check_output(f'{NETMHCPAN_LOCATION} -a {mhc} -p -f {data_path("netmhcpan_input.txt")} > {temp_output.name}', shell=True)
         print(f"netMHCpan finished {mhc}", file=sys.stderr)
         temp_output.seek(0)
-        for result, parts_entry in zip(load_netmhcpan_results(temp_output), parts.items()):
-            result_csv = ','.join(str(getattr(result, field)) for field in NetMHCpanResult.fields)
-            for _, header in parts_entry[1]:
-                print(header, end=',')
+        with open(data_path("netmhcpan_input.txt"), 'r') as input:
+            for result, header in zip(load_netmhcpan_results(temp_output), input):
+                result_csv = ','.join(str(getattr(result, field)) for field in NetMHCpanResult.fields)
+                print(header.rstrip(), end=',')
                 print(mhc, end=',')
                 print(result_csv)
-    print(f"{skipped} genomes skipped, {good} good", file=sys.stderr)
